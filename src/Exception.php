09c54df9d5dcf72e09d136db1906b1d7b1bcc38e
Abraão Zaidan <abraao@cakeweb.com.br>
2016-09-29 18:26:05 -0300

    Commit inicial

---
+++ src/Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CakeWeb;
+
+class Exception extends \Exception
+{
+	public function __construct($message, $code, Exception $previous = null)
+	{
+		parent::__construct($message, 0, $previous);
+		$this->code = $code;
+	}
+}
